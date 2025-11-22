98c39bd2d1062d0d4eade50c363bd15c5536fcd7
andrzejmusiol <andrzejmusiol88@gmail.com>
2025-11-22 12:33:08 +0100

    Add commander and basic boilerplate

---
+++ src/cli/insex.ts
@@ -0,0 +1,12 @@
+import { Command } from 'commander';
+
+const program = new Command();
+
+
+program
+    .command('create')
+    .action(() => {
+        console.log('Room creation...');
+    });
+
+program.parse();
